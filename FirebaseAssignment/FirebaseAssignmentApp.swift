ed3435df894208f91645fc9ed36a79b2ac3a8a43
noahsadir <development.noahsadir@gmail.com>
2023-10-12 10:05:56 -0400

    Initial Commit

---
+++ FirebaseAssignment/FirebaseAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAssignmentApp.swift
+//  FirebaseAssignment
+//
+//  Created by Noah Sadir on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
